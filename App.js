636a8d2ccea71c69f5435480e22d1f6b3a852f02
Quantum <autumnlegere@gmail.com>
2023-02-06 19:43:10 -0700

    added base files including package json and index js

---
+++ App.js
@@ -0,0 +1,13 @@
+// import './App.css';
+import Homepage from './components/Homepage';
+
+function App() {
+  return (
+    // <div className="bucket-app">
+    <div>
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
